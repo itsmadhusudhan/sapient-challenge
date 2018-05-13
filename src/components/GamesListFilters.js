dc5cab4d8a893d39768391e72a8b6ce9efbea932
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-05-13 14:26:42 +0530

    added genre filter functionality in gamesListFilters component

--- src/components/GamesListFilters.js
+++ src/components/GamesListFilters.js
@@ -1,32 +1,67 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setPlatform } from "../actions/filterActions";
+import { setPlatform, setGenre, setEditorsChoice,setReleaseYear,setScore } from "../actions/filterActions";
 
 class GamesListFilter extends React.Component {
-  state={
-    games:this.props.games
-  }
+  state = {
+    games: this.props.games
+  };
 
-  componentWillReceiveProps(){
-    this.setState(()=>({
-      games:this.props.games
-    }))
+  componentWillReceiveProps() {
+    this.setState(() => ({
+      games: this.props.games
+    }));
   }
 
   render() {
-    console.log(this.state)
     return (
-      <div>
-       Platform: <select onChange={e=>{
-         console.log(e.target.value);
-         this.props.dispatch(setPlatform(e.target.value));
-        }}>
-          {
-            this.state.games?[...new Set(this.props.games.map(game=>game.platform))].map(platform => (
-            <option key={platform} value={platform}>{platform}</option>
-          )):""
-        }
-        </select>
+      <div className="games__filters--bar">
+
+        <div className="games__filter">
+          Platform:
+          <select
+            onChange={e => {
+              this.props.dispatch(setPlatform(e.target.value));
+            }}
+          >
+            {this.state.games
+              ? [...new Set(this.props.games.map(game => game.platform))].map(
+                  platform => (
+                    <option key={platform} value={platform}>
+                      {platform}
+                    </option>
+                  )
+                )
+              : ""}
+          </select>
+        </div>
+
+        <div className="games__filter">
+           Genre: <select
+            onChange={e => {
+              this.props.dispatch(setGenre(e.target.value));
+            }}
+             >
+            {this.state.games
+              ? [...new Set(this.props.games.map(game => game.genre))].map(
+                  genre => (
+                    <option key={genre} value={genre}>
+                      {genre}
+                    </option>
+                  )
+                )
+              : ""}
+            </select>
+        </div>
+        <div className="games__filter">
+        
+        </div>
+        <div className="games__filter">
+        
+        </div>
+        <div className="games__filter">
+        
+        </div>
       </div>
     );
   }
@@ -35,8 +70,7 @@ class GamesListFilter extends React.Component {
 const mapStateToProps = state => {
   return {
     games: state.games,
-    filters: state.filters,
-    
+    filters: state.filters
   };
 };
 
