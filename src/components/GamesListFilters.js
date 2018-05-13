e8d439aacfaf724ed75f5cb091cef0a8f234639e
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-05-13 14:37:05 +0530

    added score filter functionality in gamesListFilters component

--- src/components/GamesListFilters.js
+++ src/components/GamesListFilters.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-import { setPlatform, setGenre, setEditorsChoice,setReleaseYear,setScore } from "../actions/filterActions";
+import {
+  setPlatform,
+  setGenre,
+  setEditorsChoice,
+  setReleaseYear,
+  setScore
+} from "../actions/filterActions";
 
 class GamesListFilter extends React.Component {
   state = {
@@ -16,14 +22,15 @@ class GamesListFilter extends React.Component {
   render() {
     return (
       <div className="games__filters--bar">
-
         <div className="games__filter">
           Platform:
           <select
+            defaultValue=""
             onChange={e => {
               this.props.dispatch(setPlatform(e.target.value));
             }}
           >
+            <option value="">All</option>
             {this.state.games
               ? [...new Set(this.props.games.map(game => game.platform))].map(
                   platform => (
@@ -37,11 +44,13 @@ class GamesListFilter extends React.Component {
         </div>
 
         <div className="games__filter">
-           Genre: <select
+          Genre:{" "}
+          <select
             onChange={e => {
               this.props.dispatch(setGenre(e.target.value));
             }}
-             >
+          >
+            <option value="">All</option>
             {this.state.games
               ? [...new Set(this.props.games.map(game => game.genre))].map(
                   genre => (
@@ -51,17 +60,35 @@ class GamesListFilter extends React.Component {
                   )
                 )
               : ""}
-            </select>
+          </select>
+        </div>
+        <div className="games__filter">
+          Score:{" "}
+          <select
+            onChange={e => {
+              this.props.dispatch(setScore(e.target.value));
+            }}
+          >
+            <option value="">All</option>
+            {this.state.games
+              ? [
+                  ...new Set(
+                    this.props.games.map(game => Math.ceil(game.score))
+                  )
+                ]
+                  .sort((a, b) => a > b)
+                  .map(score => (
+                    <option key={score} value={score}>
+                      {score}
+                    </option>
+                  ))
+              : ""}
+          </select> and above
         </div>
         <div className="games__filter">
         
         </div>
-        <div className="games__filter">
-        
-        </div>
-        <div className="games__filter">
-        
-        </div>
+        <div className="games__filter" />
       </div>
     );
   }
